4a967219cbb3efc72fce3c7bab3845e8dc24cb08
Talisha98 <101756839+Talisha98@users.noreply.github.com>
2022-03-31 21:46:58 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,46 @@
-test
+define(['pipAPI', 'https://cdn.jsdelivr.net/gh/baranan/minno-tasks@0.*/IAT/qualtrics/quiat9.js'], function(APIConstructor, iatExtension){
+    var API = new APIConstructor();
+
+	return iatExtension({
+		category1 : {
+			name : 'testing name', //Will appear in the data.
+			title : {
+				media : {word : 'Talisha test media'}, //Name of the category presented in the task.
+				css : {color:'#31940F','font-size':'2em'}, //Style of the category title.
+				height : 4 //Used to position the "Or" in the combined block.
+			}, 
+			stimulusMedia : [ //Stimuli content as PIP's media objects
+    		    {image : 'dream.jpg'}, 
+    		    {image : 'dream.jpg'}, 
+    		    {image : 'dream.jpg'}, 
+    		    {image : 'dream.jpg'}, 
+    		    {image : 'dream.jpg'}, 
+    		    {image : 'dream.jpg'}, 
+			], 
+			//Stimulus css (style)
+			stimulusCss : {color:'#31940F','font-size':'1.8em'}
+		},	
+		category2 :	{
+			name : 'White people', //Will appear in the data.
+			title : {
+				media : {word : 'White people'}, //Name of the category presented in the task.
+				css : {color:'#31940F','font-size':'2em'}, //Style of the category title.
+				height : 4 //Used to position the "Or" in the combined block.
+			}, 
+			stimulusMedia : [ //Stimuli content as PIP's media objects
+    		    {image : 'dream.jpg'}, 
+    		    {image : 'dream.jpg'}, 
+    		    {image : 'dream.jpg'}, 
+    		    {image : 'dream.jpg'}, 
+    		    {image : 'dream.jpg'}, 
+    		    {image : 'dream.jpg'}, 
+      ], 
+			//Stimulus css
+			stimulusCss : {color:'#31940F','font-size':'1.8em'}
+		},	
+
+		base_url : {//Where are your images at?
+			image : 'https://baranan.github.io/minno-tasks/images/'
+		} 
+	});
+});
